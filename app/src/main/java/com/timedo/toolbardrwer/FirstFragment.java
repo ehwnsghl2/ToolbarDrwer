94673675c69f99811a5e1b2a9a529b186302cf1e
jh <ehwnsghl2@naver.com>
2018-01-03 16:20:14 +0900

    Initial commit

---
+++ app/src/main/java/com/timedo/toolbardrwer/FirstFragment.java
@@ -0,0 +1,22 @@
+package com.timedo.toolbardrwer;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Jang Eun-ji on 2017-09-13.
+ */
+
+public class FirstFragment extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.first_fragment, container, false);
+    }
+}
---
+++ app/src/main/java/com/timedo/toolbardrwer/SecondFragment.java
@@ -0,0 +1,21 @@
+package com.timedo.toolbardrwer;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Jang Eun-ji on 2017-09-14.
+ */
+
+public class SecondFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.second_fragment, container, false);
+    }
+}
